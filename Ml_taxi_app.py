cfe926e5dc6928969e7ab7447c60c8e601771ffd
wdusman2709 <wdusman2709@gmail.com>
2026-03-18 12:19:39 +0530

    Update Ml_taxi_app.py

--- Ml_taxi_app.py
+++ Ml_taxi_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_rest_split
-from sklearn.linear_model LinearRegression
+from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score,mean_squared_error
 
 st.title("VVIT taxi Fare prediction App(End-to-End ML)")
